dc59a05fb0f7e8856f55875d1a39bf96a94cc1dc
Vitor Lobo <lobocode@gmail.com>
2019-07-10 11:50:06 -0300

    Update saitama-install.sh

--- saitama-install.sh
+++ saitama-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 set -e
 
-wget -O /usr/local/bin/saitama https://raw.githubusercontent.com/lobocode/saitama/master/saitama 
+sudo wget -O /usr/local/bin/saitama https://raw.githubusercontent.com/lobocode/saitama/master/saitama 
 
 
 
