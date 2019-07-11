7b000516eb724d75c6a2e9f086eb8d9de402af00
Vitor Lobo Ramos <lobocode@gmail.com>
2019-07-11 19:42:23 -0300

    simple saitama install script

--- saitama-install.sh
+++ saitama-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 set -e
 
-wget -O /usr/local/bin/saitama https://raw.githubusercontent.com/lobocode/saitama/master/saitama 
+wget -O /usr/local/bin/saitama https://github.com/lobocode/saitama/raw/master/bin/saitama
 chmod 755 /usr/local/bin/saitama
 
 
