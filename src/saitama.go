b4bcac7001f9ac761a479e653406f7129fb62d3e
Vitor Lobo Ramos <lobocode@gmail.com>
2019-07-12 22:40:49 -0300

    improvements

--- src/saitama.go
+++ src/saitama.go
@@ -1,20 +1,20 @@
-/* 
-Saitama a way to kill your processes with one punch. 
-developed by lobocode - lobocode@fedoraproject.org 
+/*
+Saitama a way to kill your processes with one punch.
+developed by lobocode - lobocode@fedoraproject.org
 */
 
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "log"
-    "os"
-    "path/filepath"
-    "strconv"
-    "strings"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 // args holds the commandline args
@@ -23,28 +23,26 @@ var args []string
 // findAndKillProcess in /proc/<pid>/status name and number
 func findAndKillProcess(path string, info os.FileInfo, err error) error {
 
+	//if err != nil {
+	//    //fmt.Printf("Process owner is root!\n Please use sudo.\n")
+	//    fmt.Printf("%s\n",err)
+	//    return nil
+	// }
 
-    //if err != nil {
-    //    //fmt.Printf("Process owner is root!\n Please use sudo.\n")
-    //    fmt.Printf("%s\n",err)
-    //    return nil
-   // }
+	if strings.Count(path, "/") == 3 {
+		if strings.Contains(path, "/status") {
 
+			// Let's extract the middle part of the path with the <pid> and
+			// convert the <pid> into an integer. Log an error if it fails.
+			pid, _ := strconv.Atoi(path[6:strings.LastIndex(path, "/")])
 
-    if strings.Count(path, "/") == 3 {
-        if strings.Contains(path, "/status") {
+			f, _ := ioutil.ReadFile(path)
 
-            // Let's extract the middle part of the path with the <pid> and
-            // convert the <pid> into an integer. Log an error if it fails.
-            pid, _ := strconv.Atoi(path[6:strings.LastIndex(path, "/")])
+			// Extract the process name from within the first line in the buffer
+			name := string(f[6:bytes.IndexByte(f, '\n')])
 
-            f, _ := ioutil.ReadFile(path)
-
-            // Extract the process name from within the first line in the buffer
-            name := string(f[6:bytes.IndexByte(f, '\n')])
-
-            asciArt :=
-            `⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+			asciArt :=
+				`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
             ⠀⠀⠀⠀⣠⣶⡾⠏⠉⠙⠳⢦⡀⠀⠀⠀⢠⠞⠉⠙⠲⡀⠀
             ⠀⠀⠀⣴⠿⠏⠀⠀⠀⠀⠀⠀⢳⡀⠀ ⡏⠀⠀⠀⠀ ⢷
             ⠀⠀⢠⣟⣋⡀⢀⣀⣀⡀⠀⣀⡀⣧⠀⢸⠀⠀⠀⠀⠀ ⡇
@@ -59,46 +57,46 @@ func findAndKillProcess(path string, info os.FileInfo, err error) error {
             ⣿⣿⣧⣀⣿.........⣀⣰⣏⣘⣆ 
             `
 
-            if name == args[1] {
-                
+			if name == args[1] {
 
-                proc, _ := os.FindProcess(pid)
+				proc, _ := os.FindProcess(pid)
 
+				if proc.Kill() != nil {
+					// If process owner is root
+					fmt.Printf("Please use sudo\n")
+				} else {
+					// Kill the process
+					fmt.Printf("Killing %s with one punch \n", args[1])
+					fmt.Printf("PID: %d %s %s .\n", pid, name, asciArt)
+					proc.Kill()
+				}
+			}
 
-                if proc.Kill() != nil {
-                    // If process owner is root
-                    fmt.Printf("Please use sudo\n")
-                } else {
-                    // Kill the process
-                    fmt.Printf("Killing %s with one punch \n", args[1])
-                    fmt.Printf("PID: %d %s %s .\n", pid, name, asciArt)
-                    proc.Kill()
-                }        
+			if "-l" == args[1] {
+				fmt.Printf("%s\n", name)
+			}
+		}
+	}
 
-            }
-
-        }
-    }
-
-    return nil
+	return nil
 }
 
 // main is the entry point of any go application
 func main() {
-    args = os.Args
+	args = os.Args
 
-    if len(args) != 2 {
-        log.Fatalln("Usage: saitama <processname>")
-    }
+	if len(args) != 2 {
+		log.Fatalln("Usage: saitama -p <processname> to kill with one punch \n or saitama -l to list process names ")
+	}
 
-    err := filepath.Walk("/proc", findAndKillProcess)
-    
-    if err != nil {
-        if err == io.EOF {
-            // Not an error, just a signal when we are done
-            err = nil
-        } else {
-            log.Fatal(err)
-        }
-    }
+	err := filepath.Walk("/proc", findAndKillProcess)
+
+	if err != nil {
+		if err == io.EOF {
+			// Not an error, just a signal when we are done
+			err = nil
+		} else {
+			log.Fatal(err)
+		}
+	}
 }
