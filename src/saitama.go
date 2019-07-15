a7508200f031210d20322cd0ec909176e4863eb1
Vitor Lobo Ramos <lobocode@gmail.com>
2019-07-15 17:02:57 -0300

    rewrite

--- src/saitama.go
+++ src/saitama.go
@@ -1,102 +1,52 @@
-/*
-Saitama a way to kill your processes with one punch.
-developed by lobocode - lobocode@fedoraproject.org
-*/
-
 package main
 
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 )
 
-// args holds the commandline args
 var args []string
 
-// findAndKillProcess in /proc/<pid>/status name and number
-func findAndKillProcess(path string, info os.FileInfo, err error) error {
-
-	//if err != nil {
-	//    //fmt.Printf("Process owner is root!\n Please use sudo.\n")
-	//    fmt.Printf("%s\n",err)
-	//    return nil
-	// }
+func findAndKillByName(path string, info os.FileInfo, err error) error {
 
 	if strings.Count(path, "/") == 3 {
 		if strings.Contains(path, "/status") {
+			//pid, _ := strconv.Atoi(path[6:strings.LastIndex(path, "/")])
 
-			// Let's extract the middle part of the path with the <pid> and
-			// convert the <pid> into an integer. Log an error if it fails.
-			pid, _ := strconv.Atoi(path[6:strings.LastIndex(path, "/")])
+			statesIndex, _ := ioutil.ReadFile(path)
 
-			f, _ := ioutil.ReadFile(path)
+			processName := string(statesIndex[6:bytes.IndexByte(statesIndex, '\n')])
+			//threadsNum := string(statesIndex[])
 
-			// Extract the process name from within the first line in the buffer
-			name := string(f[6:bytes.IndexByte(f, '\n')])
-
-			asciArt :=
-				`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-            ⠀⠀⠀⠀⣠⣶⡾⠏⠉⠙⠳⢦⡀⠀⠀⠀⢠⠞⠉⠙⠲⡀⠀
-            ⠀⠀⠀⣴⠿⠏⠀⠀⠀⠀⠀⠀⢳⡀⠀ ⡏⠀⠀⠀⠀ ⢷
-            ⠀⠀⢠⣟⣋⡀⢀⣀⣀⡀⠀⣀⡀⣧⠀⢸⠀⠀⠀⠀⠀ ⡇
-            ⠀⠀⢸⣯⡭⠁⠸⣛⣟⠆⡴⣻⡲⣿⠀⣸⠀⠀Oh!⠀⡇
-            ⠀⠀⣟⣿⡭⠀⠀⠀⠀⠀⢱⠀⠀⣿⠀⢹⠀⠀⠀⠀⠀ ⡇
-            ⠀⠀⠙⢿⣯⠄⠀⠀⠀⢀⡀⠀⠀⡿⠀⠀⡇⠀⠀⠀⠀⡼
-            ⠀⠀⠀⠀⠹⣶⠆⠀⠀⠀⠀⠀⡴⠃⠀⠀⠘⠤⣄⣠⠞⠀
-            ⠀⠀⠀⠀⠀⢸⣷⡦⢤⡤⢤⣞⣁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-            ⠀⠀⢀⣤⣴⣿⣏⠁⠀⠀⠸⣏⢯⣷⣖⣦⡀⠀⠀⠀⠀⠀⠀
-            ⢀⣾⣽⣿⣿⣿⣿⠛⢲⣶⣾⢉⡷⣿⣿⠵⣿⠀⠀⠀⠀⠀⠀
-            ⣼⣿⠍⠉⣿⡭⠉⠙⢺⣇⣼⡏⠀⠀⠀⣄⢸⠀⠀⠀⠀⠀⠀
-            ⣿⣿⣧⣀⣿.........⣀⣰⣏⣘⣆ 
-            `
-
-			if name == args[1] {
-
-				proc, _ := os.FindProcess(pid)
-
-				if proc.Kill() != nil {
-					// If process owner is root
-					fmt.Printf("Please use sudo\n")
-				} else {
-					// Kill the process
-					fmt.Printf("Killing %s with one punch \n", args[1])
-					fmt.Printf("PID: %d %s %s .\n", pid, name, asciArt)
-					proc.Kill()
-				}
-			}
-
-			if "-l" == args[1] {
-				fmt.Printf("%s\n", name)
-			}
+			//fmt.Printf("%s", pid)
+			//fmt.Printf("%s", f)
+			fmt.Println(processName)
 		}
 	}
 
 	return nil
 }
 
-// main is the entry point of any go application
 func main() {
+
 	args = os.Args
 
-	if len(args) != 2 {
-		log.Fatalln("Usage: saitama -p <processname> to kill with one punch \n or saitama -l to list process names ")
-	}
+	debugFunc := filepath.Walk("/proc", findAndKillByName)
 
-	err := filepath.Walk("/proc", findAndKillProcess)
+	fmt.Println(debugFunc)
 
-	if err != nil {
-		if err == io.EOF {
-			// Not an error, just a signal when we are done
-			err = nil
-		} else {
-			log.Fatal(err)
-		}
-	}
+	/* 	err := filepath.Walk("/proc", findAndKillByName)
+
+	   	if err != nil {
+	   		if err == io.EOF {
+	   			// Not an error, just a signal when we are done
+	   			err = nil
+	   		} else {
+	   			log.Fatal(err)
+	   		}
+	   	} */
 }
