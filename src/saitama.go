74ab22b4cdaed11ec6f6b34fba3f7d116f2805e1
Vitor Lobo Ramos <lobocode@gmail.com>
2019-07-16 06:45:59 -0300

    rewrite code

--- src/saitama.go
+++ src/saitama.go
@@ -1,52 +1,94 @@
+// killprocess project main.go
 package main
 
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
+// args holds the commandline args
 var args []string
 
-func findAndKillByName(path string, info os.FileInfo, err error) error {
+func findAndKillProcess(path string, info os.FileInfo, err error) error {
 
+	// We are only interested in files with a path looking like /proc/<pid>/status.
 	if strings.Count(path, "/") == 3 {
 		if strings.Contains(path, "/status") {
-			//pid, _ := strconv.Atoi(path[6:strings.LastIndex(path, "/")])
 
-			statesIndex, _ := ioutil.ReadFile(path)
+			pid, _ := strconv.Atoi(path[6:strings.LastIndex(path, "/")])
 
-			processName := string(statesIndex[6:bytes.IndexByte(statesIndex, '\n')])
-			//threadsNum := string(statesIndex[])
+			readIndex, _ := ioutil.ReadFile(path)
 
-			//fmt.Printf("%s", pid)
-			//fmt.Printf("%s", f)
-			fmt.Println(processName)
+			// Extract the process name from within the first line in the buffer
+			processName := string(readIndex[6:bytes.IndexByte(readIndex, '\n')])
+
+			oh :=
+				`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+		⠀⠀⠀⠀⣠⣶⡾⠏⠉⠙⠳⢦⡀⠀⠀⠀⢠⠞⠉⠙⠲⡀⠀
+		⠀⠀⠀⣴⠿⠏⠀⠀⠀⠀⠀⠀⢳⡀⠀ ⡏⠀⠀⠀⠀ ⢷
+		⠀⠀⢠⣟⣋⡀⢀⣀⣀⡀⠀⣀⡀⣧⠀⢸⠀⠀⠀⠀⠀ ⡇
+		⠀⠀⢸⣯⡭⠁⠸⣛⣟⠆⡴⣻⡲⣿⠀⣸⠀⠀Oh!⠀⡇
+		⠀⠀⣟⣿⡭⠀⠀⠀⠀⠀⢱⠀⠀⣿⠀⢹⠀⠀⠀⠀⠀ ⡇
+		⠀⠀⠙⢿⣯⠄⠀⠀⠀⢀⡀⠀⠀⡿⠀⠀⡇⠀⠀⠀⠀⡼
+		⠀⠀⠀⠀⠹⣶⠆⠀⠀⠀⠀⠀⡴⠃⠀⠀⠘⠤⣄⣠⠞⠀
+		⠀⠀⠀⠀⠀⢸⣷⡦⢤⡤⢤⣞⣁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+		⠀⠀⢀⣤⣴⣿⣏⠁⠀⠀⠸⣏⢯⣷⣖⣦⡀⠀⠀⠀⠀⠀⠀
+		⢀⣾⣽⣿⣿⣿⣿⠛⢲⣶⣾⢉⡷⣿⣿⠵⣿⠀⠀⠀⠀⠀⠀
+		⣼⣿⠍⠉⣿⡭⠉⠙⢺⣇⣼⡏⠀⠀⠀⣄⢸⠀⠀⠀⠀⠀⠀
+		⣿⣿⣧⣀⣿.........⣀⣰⣏⣘⣆ 
+		`
+
+			switch args[1] {
+			case "--help", "-h":
+				log.Fatalln("\nUsage: saitama [OPTION] <processname>\nKill process with one punch\n\nMandatory arguments\n\n-h  --help	display this help and exit\n-l  --list	list process by name\n-p  --punch <processname> punch process by name")
+			case "-l", "--list":
+				fmt.Printf("%s\n", processName)
+			case "-p", "--punch":
+				if len(args) == 2 {
+					log.Fatalln("\nMissing operand\nTry 'saitama --help' for more information")
+					return io.EOF
+				} else if processName == args[2] {
+
+					proc, _ := os.FindProcess(pid)
+
+					if proc.Kill() != nil {
+						fmt.Printf("\nWarning: This process owner is 'root'\nPlease use 'sudo'\n")
+					} else {
+						fmt.Printf("Killing %s with one punch \n", args[2])
+						fmt.Printf("PID: %d %s %s .\n", pid, processName, oh)
+						proc.Kill()
+
+						// if error
+						return io.EOF
+					}
+				}
+
+			default:
+				log.Fatalln("Error")
+			}
+
 		}
 	}
 
 	return nil
 }
 
+// main is the entry point of any go application
 func main() {
-
 	args = os.Args
 
-	debugFunc := filepath.Walk("/proc", findAndKillByName)
+	if len(args) == 1 {
+		log.Fatalln("\nSaitama: missing operand\nTry 'saitama --help' for more information")
+	}
 
-	fmt.Println(debugFunc)
+	debugCall := filepath.Walk("/proc", findAndKillProcess)
+	fmt.Printf("%s", debugCall)
 
-	/* 	err := filepath.Walk("/proc", findAndKillByName)
-
-	   	if err != nil {
-	   		if err == io.EOF {
-	   			// Not an error, just a signal when we are done
-	   			err = nil
-	   		} else {
-	   			log.Fatal(err)
-	   		}
-	   	} */
 }
