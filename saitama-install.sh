858a70ae0bf7fd652c5389f54b9bd5d830cf828d
Vitor Lobo Ramos <lobocode@gmail.com>
2025-02-24 08:18:27 -0300

    Add installation script for saitama binary

--- saitama-install.sh
+++ saitama-install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
-SAITAMA_URL="https://github.com/lobocode/saitama/raw/master/bin/saitama"
+SAITAMA_URL="https://github.com/scovl/saitama/raw/master/bin/saitama"
 SAITAMA_PATH="/usr/local/bin/saitama"
 
 wget -O "$SAITAMA_PATH" "$SAITAMA_URL"
 chmod 755 "$SAITAMA_PATH"
+
+echo "Saitama installed successfully!"
+echo "Run 'saitama' to get started."
+
+
